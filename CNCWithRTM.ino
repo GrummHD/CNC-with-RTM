665b2fa588c8b240ea77a3c15293320ce59d75ed
JsPr <JsPr>
2016-10-25 21:03:05 +0200

    >Serial.test

--- CNCWithRTM.ino
+++ CNCWithRTM.ino
@@ -1,27 +1,29 @@
 
 volatile int i;
+volatile float input_line[4][6]; 
 void setup() {
 cli();
 TCCR1A = 0;
 TCCR1B = 0;
 TCNT1 = 0;
-OCR1A = 249;
-TCCR1B |= (0<<CS12) | (0<<CS11) | (1<<CS10);
+OCR1A = 15600;
+TCCR1B |= (1<<CS12) | (0<<CS11) | (1<<CS10);
 TCCR1B |= (1<<WGM12);
 TIMSK1 |= (1<<OCIE1A);
 sei();
 Serial.begin(9600);
-pinMode(13,1);
 }
 
 void loop() {
-
+delay(1000);
+Serial.println(i);
 }
-ISR(TIMER1_COMP_vect)
-{
-if(i== 5000);
+ISR(TIMER1_COMPA_vect){
+  i++;
+}
+
 
 
   
-}
+
 
